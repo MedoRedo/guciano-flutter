0770a35c960509cdb5efdc71145f093ee651c1ba
HusseinElFeky <hussein.feky@gmail.com>
2021-12-28 02:24:28 +0200

    Add order status to order model

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -3,6 +3,7 @@ class Order {
   int orderNumber;
   Delivery deliveryOption;
   Payment paymentOption;
+  OrderStatus orderStatus;
   int itemsCount;
   DateTime timeStamp;
   num totalPrice;
@@ -12,10 +13,24 @@ class Order {
     required this.orderNumber,
     required this.deliveryOption,
     required this.paymentOption,
+    required this.orderStatus,
     required this.itemsCount,
     required this.timeStamp,
     required this.totalPrice,
   });
+
+  static OrderStatus mapToOrderStatus(String orderStatus) {
+    switch (orderStatus) {
+      case 'in_progress':
+        return OrderStatus.inProgress;
+      case 'delivering':
+        return OrderStatus.delivering;
+      case 'done':
+        return OrderStatus.done;
+      default:
+        throw UnsupportedError('$orderStatus is unsupported.');
+    }
+  }
 
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
@@ -26,6 +41,7 @@ class Order {
       paymentOption: json['payment_option'] == 'credit_card'
           ? Payment.creditCard
           : Payment.cash,
+      orderStatus: mapToOrderStatus(json['order_status']),
       itemsCount: json['items_count'],
       timeStamp: DateTime.parse(json['timestamp'].toDate().toString()),
       totalPrice: double.parse((json['total_price'].toString())),
@@ -36,3 +52,5 @@ class Order {
 enum Delivery { dorm, kiosk }
 
 enum Payment { cash, creditCard }
+
+enum OrderStatus { inProgress, delivering, done }
