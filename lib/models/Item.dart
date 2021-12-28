18a1b21119e4edb1ca253f5d6576710801d91b8e
Samer Ghaly <samer.ghaly2017@gmail.com>
2021-12-28 21:57:29 +0200

    Item ,category classes in models categories_repo.dart

--- lib/models/Item.dart
+++ lib/models/Item.dart
@@ -3,17 +3,23 @@ class Item {
   String name;
   int price;
   String itemId;
+  double rating;
+  int ratingNum;
   Item(
       {required this.imgPath,
       required this.name,
       required this.price,
-      required this.itemId});
+      required this.itemId,
+      required this.rating,
+      required this.ratingNum});
 
   factory Item.fromJson(Map<String, dynamic> json) {
     return Item(
         imgPath: json['image'],
         name: json['name'],
         price: json['price'],
-        itemId: json['id']);
+        itemId: json['id'],
+        rating: json['rating'] == null ? 5 : json['rating'],
+        ratingNum: json['ratingNum'] == null ? 0 : json['ratingNum']);
   }
 }
