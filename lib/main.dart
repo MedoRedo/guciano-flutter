7b81f84e5bd9b3104623a4d01eb7ceaa3895c832
MedoRedo <moreda.elsayed@gmail.com>
2022-01-18 23:34:08 +0200

    screenshots floder

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:guciano_flutter/pages/login_page.dart';
 import 'package:guciano_flutter/providers/cart_provider.dart';
 import 'package:guciano_flutter/providers/home_page_provider.dart';
 import 'package:guciano_flutter/routes.dart';
-import 'package:guciano_flutter/widgets/counter_widget.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -57,57 +56,6 @@ class MyApp extends StatelessWidget {
           routes: routes,
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-    });
-  }
-
-  void decrement() {
-    setState(() {
-      if (count != 0) count--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Counter(
-        count: count,
-        increment: increment,
-        decrement: decrement,
-      )),
     );
   }
 }
